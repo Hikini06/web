dcd6af7043641ef9f741b2cce49cd46e6260edd6
Hikini06 <lgeml07@gmail.com>
2024-11-10 17:02:02 +0700

    slied hình ảnh

--- home.js
+++ home.js
@@ -55,3 +55,29 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+// trình chiếu hình ảnh
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].classList.add("active");
+  setTimeout(showSlides, 5000); // Thay đổi ảnh mỗi 5 giây
+}
+
+function plusSlides(n) {
+  slideIndex += n;
+  let slides = document.getElementsByClassName("mySlides");
+  if (slideIndex > slides.length) {slideIndex = 1}
+  if (slideIndex < 1) {slideIndex = slides.length}
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+  slides[slideIndex-1].classList.add("active");
+}
