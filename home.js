56a3fe39be5d4112c8651b31a49d3eae97cac83f
Hikini06 <lgeml07@gmail.com>
2024-11-12 04:42:14 +0700

    Đang sửa

--- home.js
+++ home.js
@@ -58,6 +58,7 @@ document.addEventListener("DOMContentLoaded", function () {
 // trình chiếu hình ảnh
 let slideIndex = 0;
 showSlides();
+const slides = document.getElementsByClassName("mySlides");
 
 function showSlides() {
   let i;
@@ -73,17 +74,21 @@ function showSlides() {
   setTimeout(showSlides, 5000); // Thay đổi ảnh mỗi 5 giây
 }
 
-// function plusSlides(n) {
-//   slideIndex += n;
-//   let slides = document.getElementsByClassName("mySlides");
-//   if (slideIndex > slides.length) {
-//     slideIndex = 1;
-//   }
-//   if (slideIndex < 1) {
-//     slideIndex = slides.length;
-//   }
-//   for (let i = 0; i < slides.length; i++) {
-//     slides[i].classList.remove("active");
-//   }
-//   slides[slideIndex - 1].classList.add("active");
-// }
+function plusSlides(n) {
+  slideIndex += n;
+  let slides = document.getElementsByClassName("mySlides");
+  if (slideIndex > slides.length) {
+    slideIndex = 1 ;
+  }
+  if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+  slides[slideIndex - 1].classList.add("active");
+}
+// Khởi tạo trình chiếu khi trang được tải
+document.addEventListener("DOMContentLoaded", function() {
+  showSlides();
+});
