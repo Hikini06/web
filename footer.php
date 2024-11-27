6b8197d3519b6e85b6e37d1e494be63895d70410
Hikini06 <lgeml07@gmail.com>
2024-11-27 16:37:45 +0700

    chua lam gi ca

--- footer.php
+++ footer.php
@@ -8,6 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/1081860f2a.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="footer.css" />
+    
 </head>
 <body>
     <!-- FOOTER START -->
@@ -98,6 +99,7 @@
       <h5 class="footer-h">Tiệm hoa MiMi - Since 2020</h5>
     </footer>
     <!-- FOOTER END -->
+
 
     <script src="footer.js" defer></script>
 </body>
